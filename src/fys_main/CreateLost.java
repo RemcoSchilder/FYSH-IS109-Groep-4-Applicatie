b8e7dd02c505dc43b5261f8db14d49f18b49235b
Menno <Menno@145.28.152.252>
2016-11-29 12:41:08 +0100

    CreateLost update

--- src/fys_main/CreateLost.java
+++ src/fys_main/CreateLost.java
@@ -2,6 +2,7 @@ package fys_main;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -40,6 +41,13 @@ public class CreateLost {
         
         Text contact = new Text("Contact:");
         contact.getStyleClass().add("subheading");
+        
+        
+        
+        /* Create buttons */
+        Button cancel = new Button("Cancel");
+        Button next = new Button("Next");
+        Button addLostLuggage = new Button("Add lost luggage");
         
         
         
