1ceae88d7bbbe407504c034d245c1086aeee0b2b
Menno <Menno@145.28.152.252>
2016-11-29 13:53:52 +0100

    Idk

--- src/fys_main/HB_CreateLost.java
+++ src/fys_main/HB_CreateLost.java
@@ -1,5 +1,7 @@
 package fys_main;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
@@ -19,6 +21,12 @@ public class HB_CreateLost {
     }
     
     public static GridPane getScreen() {
+        GridPane screenOne = screenOne();
+        
+        return screenOne;
+    }
+    
+    public static GridPane screenOne() {
         /* Create grid pane */
         GridPane screen = new GridPane();
         screen.setAlignment(Pos.CENTER);
@@ -28,8 +36,8 @@ public class HB_CreateLost {
         
         
         /* Create all subheadings */
-        Text nameText = new Text("Name:");
-        nameText.getStyleClass().add("subheading");
+        Text name = new Text("Name:");
+        name.getStyleClass().add("subheading");
         
         Text mainAddress = new Text("Main address:");
         mainAddress.getStyleClass().add("subheading");
@@ -47,6 +55,14 @@ public class HB_CreateLost {
         Button next = new Button("Next");
         Button addLostLuggage = new Button("Add lost luggage");
         
+        next.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                next.setText("Yeah");
+
+            }
+        });
+        
         
         
         /* Create all labels & inputs */
@@ -57,21 +73,35 @@ public class HB_CreateLost {
         TextField lastNameT = new TextField();
         
         Label streetL = new Label("Street:");
+        Label street2L = new Label("Street:");
         TextField streetT = new TextField();
+        TextField street2T = new TextField();
         
         Label cityL = new Label("City:");
+        Label city2L = new Label("City:");
         TextField cityT = new TextField();
+        TextField city2T = new TextField();
         
         Label zipCodeL = new Label("Zip code:");
+        Label zipCode2L = new Label("Zip code:");
         TextField zipCodeT = new TextField();
+        TextField zipCode2T = new TextField();
         
         Label countryL = new Label("Country:");
+        Label country2L = new Label("Country:");
         TextField countryT = new TextField();
+        TextField country2T = new TextField();
+        
+        Label emailL = new Label("Email:");
+        TextField emailT = new TextField();
+        
+        Label telephoneL = new Label("Telephone:");
+        TextField telephoneT = new TextField();
         
         
         
         /* Add everything to the grid */
-        screen.add(nameText, 0, 0);
+        screen.add(name, 0, 0);
         
         screen.add(firstNameL, 0, 1);
         screen.add(firstNameT, 1, 1);
@@ -80,18 +110,37 @@ public class HB_CreateLost {
         screen.add(lastNameT, 1, 2);
         
         screen.add(mainAddress, 0, 4);
+        screen.add(mainAddress2, 3, 4);
         
         screen.add(streetL, 0, 5);
         screen.add(streetT, 1, 5);
+        screen.add(street2L, 3, 5);
+        screen.add(street2T, 4, 5);
         
         screen.add(cityL, 0, 6);
         screen.add(cityT, 1, 6);
+        screen.add(city2L, 3, 6);
+        screen.add(city2T, 4, 6);
         
         screen.add(zipCodeL, 0, 7);
         screen.add(zipCodeT, 1, 7);
+        screen.add(zipCode2L, 3, 7);
+        screen.add(zipCode2T, 4, 7);
         
         screen.add(countryL, 0, 8);
         screen.add(countryT, 1, 8);
+        screen.add(country2L, 3, 8);
+        screen.add(country2T, 4, 8);
+        
+        screen.add(contact, 0, 10);
+        
+        screen.add(emailL, 0, 11);
+        screen.add(emailT, 1, 11);
+        
+        screen.add(telephoneL, 0, 12);
+        screen.add(telephoneT, 1, 12);
+        
+        screen.add(next, 4, 14);
         
         
         
