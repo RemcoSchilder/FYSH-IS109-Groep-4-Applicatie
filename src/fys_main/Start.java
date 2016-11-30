17016ae7debf78635eca9603c08ad7de7a7d961e
thijst1997 <thijs-97@hotmail.com>
2016-11-30 23:19:14 +0100

    van gridpane naar borderpane werkt plus button functies, sidebar homepage_baliemedewerker en hb_createlost werkt perfect

--- src/fys_main/Start.java
+++ src/fys_main/Start.java
@@ -5,10 +5,11 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
 /**
@@ -17,7 +18,7 @@ import javafx.scene.layout.VBox;
  */
 public class Start {
 
-    private static GridPane pane = new GridPane();
+    private static BorderPane pane = new BorderPane();
 
     private static Button btnBalie = new Button("Baliemedewerker");
     private static Button btnSysteem = new Button("Systemeebeheer");
@@ -26,15 +27,52 @@ public class Start {
     
             
 
-    public static GridPane getScreen() {
+    public static BorderPane getScreen() {
 
-        pane.setAlignment(Pos.CENTER);
-        pane.setHgap(10);
-        pane.setVgap(10);
+//       pane.getChildren().setAlignment(Pos.CENTER);
+//       pane.setHgap(10);
+//        pane.setVgap(10);
+
+
+        //       dit moet er nog staan na de pull
+
         
-        start.getChildren().addAll(btnBalie, btnSysteem, btnManager);
-        pane.add(start, 50, 50);
+        GridPane grid = new GridPane();
+        grid.setMinSize(150, 150);
+        grid.setHgap(5);
+        grid.setVgap(5);
+        grid.setPadding(new Insets(200, 200, 200, 200));
         
+        grid.add(btnBalie, 30, 20);
+        grid.add(btnSysteem, 30, 25);
+        grid.add(btnManager, 30, 30);
+       
+        
+        
+        GridPane grid2 = new GridPane();
+        grid2.setMinSize(150, 150);
+        grid2.setHgap(5);
+        grid2.setVgap(5);
+        grid2.setPadding(new Insets(100, 100, 100, 100));
+        
+        Image image = new Image("http://www.corendonairlines.nl/corendon_logo.jpg", 600, 200, false, false);
+
+        ImageView iv1 = new ImageView(image);
+        iv1.setImage(image);
+        iv1.setPreserveRatio(true);
+        iv1.setSmooth(true);
+        iv1.setCache(true);
+
+        
+        VBox box = new VBox();
+        box.setAlignment(Pos.TOP_CENTER);
+        box.getChildren().add(iv1);
+        grid2.getChildren().add(box);
+        
+        pane.setCenter(grid);
+        pane.setTop(grid2);
+       
+         
         btnBalie.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
@@ -61,21 +99,9 @@ public class Start {
         
 
 
-        Image image = new Image("http://www.corendonairlines.nl/corendon_logo.jpg", 600, 200, false, false);
+        
 
-        ImageView iv1 = new ImageView(image);
-        iv1.setImage(image);
-        iv1.setPreserveRatio(true);
-        iv1.setSmooth(true);
-        iv1.setCache(true);
-
-        //sceneR.setFill(Color.WHITE);
-        VBox box = new VBox();
-        box.setAlignment(Pos.TOP_CENTER);
-        box.getChildren().add(iv1);
-        pane.getChildren().add(box);
-//
-//        primaryStage.getIcons().add(new Image("https://www.corendon.be/apple-touch-icon-152x152.png"));
+     
         return pane;
     }
 }
