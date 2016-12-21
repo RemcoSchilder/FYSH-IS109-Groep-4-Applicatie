f4bc5ea8f89782ca9234889c153ce454124377c3
thijst1997 <thijs-97@hotmail.com>
2016-12-21 10:04:17 +0100

    MENNO DOE FF EDIT DAN!

--- src/fys_main/Homepage_Baliemedewerker.java
+++ src/fys_main/Homepage_Baliemedewerker.java
@@ -1,10 +1,21 @@
 package fys_main;
 
+import static fys_main.FYS_LostFound.pane;
+import fys_main.HB_SearchBaggage.TableLuggage;
+import java.util.Optional;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
@@ -16,17 +27,22 @@ public class Homepage_Baliemedewerker {
 
     //aanmaken allover borderpane
     private static BorderPane pane = new BorderPane();
+    private static GridPane grid = new GridPane();
+
+   private static TableView<TableLuggage> table = new TableView<>();
+    private static ObservableList<TableLuggage> data = FXCollections.observableArrayList();
 
     //sidebar Menu Buttons
     private static Label createL = new Label("Create");
     private static Button lost = new Button("Lost baggage");
     private static Button found = new Button("Found baggage");
-    private static Button edit = new Button ("Edit");
+    private static Button edit = new Button("Edit");
     private static Label searchL = new Label("Search");
     private static Button baggage = new Button("Baggage");
     private static Button logout = new Button("Log out");
-    
-    
+    private static HBox hbox = new HBox();
+    private static VBox vbox = new VBox();
+
     //methode voor het scherm
     public static VBox vboxLeft() {
         VBox vbox = new VBox();
@@ -46,43 +62,45 @@ public class Homepage_Baliemedewerker {
 
         //style voor de vbox
         vbox.getStyleClass().add("vbox");
-        
+
         return vbox;
     }
-   
+
     public static HBox HboxBottom() {
-        HBox hbox = new HBox();
-        
+
         //buttons
         logout.setMinSize(230, 48);
         edit.setMinSize(230, 48);
         //alles wordt in de vbox gestopt
-        hbox.getChildren().addAll(logout, edit);
+        hbox.getChildren().addAll(logout);
 
         //style voor de vbox
         hbox.getStyleClass().add("vbox");
         //VBox.setMargin(edit, new Insets(566, 0, 0, 0));
-       
-       
-       
+
         return hbox;
     }
-    
-    
-    
-    
+
+    public static VBox vboxRight() {
+
+        //style voor de vbox
+        vbox.getStyleClass().add("vbox");
+
+        return vbox;
+    }
+
     public static BorderPane getScreen() {
 
         pane.setLeft(vboxLeft());
         pane.setBottom(HboxBottom());
-        
-        
+        pane.setRight(vboxRight());
+
         //eventhander knop lost openen lost invoer
         lost.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 pane.setCenter(HB_CreateLost.getScreen());
-
+                hbox.getChildren().remove(edit);
             }
         });
 
@@ -90,7 +108,7 @@ public class Homepage_Baliemedewerker {
             @Override
             public void handle(ActionEvent event) {
                 pane.setCenter(HB_CreateFound.getScreen());
-
+                hbox.getChildren().remove(edit);
             }
         });
 
@@ -98,21 +116,47 @@ public class Homepage_Baliemedewerker {
             @Override
             public void handle(ActionEvent event) {
                 pane.setCenter(HB_SearchBaggage.getScreen());
+                hbox.getChildren().add(edit);
 
             }
         });
 
-        //eventhandler van logout knop naar start
         logout.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-
                 pane.getChildren().clear();
                 pane.getScene().setRoot(Start.getScreen());
 
             }
         });
+        
+        
+        edit.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                pane.setCenter(getScreenOne());
+                hbox.getChildren().remove(edit);
+
+            }
+        });
+       
+    
+    return pane;
+    }
+         
+       public static BorderPane getScreenOne() {
+        
+           
+           
+           
+           
+           
+           
+        
 
         return pane;
     }
+
+    
+
 }
